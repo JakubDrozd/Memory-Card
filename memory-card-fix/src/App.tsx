e2ce4a06c80ac67ab1594258323978b670a10105
Veanty <plmulticraftpl@gmail.com>
2022-08-22 19:09:58 +0200

    Feat: add base for project display

---
+++ memory-card-fix/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/App.css';
+import { Playboard } from './components/Playboard';
+
+function App() {
+  return (
+    <Playboard></Playboard>
+  );
+}
+
+export default App;
