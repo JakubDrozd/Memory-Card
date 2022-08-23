e175b74bacf3c874437f70111bcb575d8e1d9e64
Veanty <plmulticraftpl@gmail.com>
2022-08-23 17:47:04 +0200

    Feat: add new maxScore to display best score

--- memory-card-fix/src/components/Scoreboard.tsx
+++ memory-card-fix/src/components/Scoreboard.tsx
@@ -2,6 +2,6 @@ import "../styles/Scoreboard.css"
 
 export function Scoreboard(props: any){
     return (
-        <div className="score-container"><h1>Score: {props.score}</h1></div>
+        <div className="score-container"><h1>Score: {props.score} | Best score: {props.bestScore}</h1></div>
     )
 }
