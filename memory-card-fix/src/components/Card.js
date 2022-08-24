8170313c63d4f069d400d014a9d56c60e0a26fdd
Veanty <plmulticraftpl@gmail.com>
2022-08-24 17:26:57 +0200

    Refactor: fix call function onClick

--- memory-card-fix/src/components/Card.js
+++ memory-card-fix/src/components/Card.js
@@ -3,7 +3,7 @@ import "../styles/Card.css";
 
 export function Card({ name, handleCardClick }) {
   return (
-    <div className="card" {() => handleCardClick(name)}>
+    <div className="card" onClick={() => handleCardClick(name)}>
       <h1>{name}</h1>
     </div>
   );
