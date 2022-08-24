a70d399fbfb4ef0111936b6caca2234eb0651915
Veanty <plmulticraftpl@gmail.com>
2022-08-24 18:10:32 +0200

    Refactor: delete cards array from state and made free array, useEffect to get bestScore, quality of code changes

--- memory-card-fix/src/components/Playboard.js
+++ memory-card-fix/src/components/Playboard.js
@@ -3,67 +3,45 @@ import { Card } from "./Card";
 import "../styles/Playboard.css";
 import { Scoreboard } from "./Scoreboard";
 
-export function Playboard(props) {
+const shuffleArray = (array) => {
+  return [...array].sort(() => Math.random() - 0.5);
+};
+
+const initializeCards = ["A", "B", "C", "D", "E", "F"];
+
+export function Playboard() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-
   const [clicked, setClicked] = useState([]);
-  const [cards, setCards] = useState([
-    <Card name="A" key={1} handleCardClick={handleCardClick}></Card>,
-    <Card name="B" key={2} handleCardClick={handleCardClick}></Card>,
-    <Card name="C" key={3} handleCardClick={handleCardClick}></Card>,
-    <Card name="D" key={4} handleCardClick={handleCardClick}></Card>,
-    <Card name="E" key={5} handleCardClick={handleCardClick}></Card>,
-    <Card name="F" key={6} handleCardClick={handleCardClick}></Card>,
-  ]);
-
-  const increment = () => {
-    setScore((c) => c + 1);
-  };
-
-  function checkScore() {
-    if (score > bestScore) {
-      setBestScore(score);
-    }
-  }
+  const [cards, setCards] = useState(initializeCards);
 
   useEffect(() => {
-    setCards(shuffleArray(cards));
-  }, []);
-
-  useEffect(() => {
-    setCards(shuffleArray(cards));
+    setBestScore((bestScore) => Math.max(bestScore, score));
   }, [score]);
 
-  useEffect(() => {
-    setClicked([]);
-  }, []);
-
-  function handleCardClick(e) {
-    console.log(clicked);
-    if (clicked.includes(e.target.querySelector("h1").textContent)) {
-      console.log(clicked);
+  function handleCardClick(name) {
+    if (clicked.includes(name)) {
       resetGame();
     } else {
-      increment();
-      clicked.push(e.target.querySelector("h1").textContent);
-      console.log(clicked);
+      setScore((c) => c + 1);
+      setClicked((c) => [...c, name]);
+      setCards((cards) => shuffleArray(cards));
     }
   }
 
   function resetGame() {
     setScore(0);
     setClicked([]);
-    console.log(clicked);
+    setCards(initializeCards);
   }
-
-  const shuffleArray = (array) => {
-    return [...array].sort(() => Math.random() - 0.5);
-  };
 
   return (
     <div>
-      <div className="container">{cards}</div>
+      <div className="container">
+        {cards.map((name) => (
+          <Card name={name} key={name} handleCardClick={handleCardClick}></Card>
+        ))}
+      </div>
       <Scoreboard score={score} bestScore={bestScore}></Scoreboard>
     </div>
   );
